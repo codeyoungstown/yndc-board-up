a2bc2c840a0f4c70770b3aaf8698e569ea0c8f2f
Nicholas Serra <nicks@ydekproductions.com>
2014-10-26 22:26:35 -0400

    url for slugs

--- yndc/apps/board/urls.py
+++ yndc/apps/board/urls.py
@@ -4,6 +4,6 @@ urlpatterns = patterns('board.views',
     url(r'^/?$', 'list', name='list'),
     url(r'^add/?$', 'add_house', name='add'),
     url(r'^neighborhood/(?P<neighborhood_slug>[\w_]+)/?$', 'neighborhood'),
-    url(r'^house/(?P<house_slug>[\w_]+)/?$', 'house', name='house'),
+    url(r'^house/(?P<house_slug>[\w-]+)/?$', 'house', name='house'),
     url(r'^login/?$', 'login_user'),
 )
