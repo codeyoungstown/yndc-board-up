694673969fb2c1d5c9404fce895fdb9130052207
Nicholas Serra <nicks@ydekproductions.com>
2014-12-04 21:17:53 -0500

    Prep for heroku

--- yndc/settings/__init__.py
+++ yndc/settings/__init__.py
@@ -1,6 +1,18 @@
+import os
+
 from default import *
+
 
 try:
     from local import *
 except ImportError:
     pass
+
+# There's probably a much better way to do this.
+# FIXME nicholasserra
+if os.environ.get('ENVIRONMENT') == 'production':
+    try:
+        from production import *
+    except ImportError:
+        pass
+
---
+++ yndc/settings/production.py
@@ -0,0 +1,20 @@
+import os
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': os.environ['DATABASE_HOST'],
+        'PORT': os.environ['DATABASE_PORT'],
+    }
+}
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+
+STATIC_URL = os.environ['STATIC_URL']
